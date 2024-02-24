f87b3d5afcc801c8937161534dff5827d776057d
Alpay <138126622+alpaykalkanci@users.noreply.github.com>
2024-02-24 21:56:47 +0300

    Update Main.java

--- YaricapiVerilenDaireninAlaniVeCevresiniBulma/Main.java
+++ YaricapiVerilenDaireninAlaniVeCevresiniBulma/Main.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        double pi = 3.14;
+        double r, alan, cevre;
+
+
+        System.out.print("Dairenin yarı çapını giriniz: ");
+        r = input.nextDouble();
+
+        alan = pi * r * r;
+        cevre = 2 * pi * r;
+
+        System.out.println("Yarıçapı " + r + " olan dairenin alanı: " + alan);
+        System.out.print("Yarıçapı " + r + " olan dairenin çevresi: " +  + cevre);
+
+    }
+}
