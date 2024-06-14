145f89894e3f388e4bb6ce49950fd3b382ceca72
Alpay <138126622+alpaykalkanci@users.noreply.github.com>
2024-06-14 22:37:04 +0300

    Update and rename KosulluIfadelerVeKodBloklari to KosulluIfadelerVeKodBlo/Main.java

---
+++ KosulluIfadelerVeKodBlo/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        double ortalama = 0;
+
+        //Kullanıcıdan veri alma
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Not ortalamasını giriniz: ");
+        ortalama = input.nextDouble();
+
+        System.out.println(ortalama > 45 ? "Geçti" : "Kaldı");
+
+        boolean a = true, b = false;
+        System.out.println((a && b) || (a || b));
+
+    
+    }
+}
